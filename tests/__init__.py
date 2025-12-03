77015998cc0bfe164234ab832c13834d817e240c
tavishh <mgistud@gmail.com>
2025-12-02 23:43:44 -0800

    added new tests - scores
    Signed-off-by: tavishh <mgistud@gmail.com>

---
+++ tests/__init__.py
@@ -0,0 +1,17 @@
+"""
+Tests Package for Fish-O-Mania.
+
+This package contains unit tests for all game modules.
+
+Run all tests from project root:
+    python -m unittest discover tests
+
+Run a specific test file:
+    python -m unittest tests.test_scores
+
+Run a specific test class:
+    python -m unittest tests.test_scores.TestUpdateHighScore
+
+Run a specific test method:
+    python -m unittest tests.test_scores.TestUpdateHighScore.test_new_high_score
+"""
